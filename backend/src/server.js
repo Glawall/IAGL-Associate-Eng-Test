cc41e5a3f413da9db3df75aa0499c86ab0994b2c
Glawall <glawall217@gmail.com>
2024-11-15 15:40:32 +0000

    Completes addTodo on api/todos endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const repository = require('./repository/todo');
-const todoService = require('./service/todo')(repository);
+const express = require("express");
+const cors = require("cors");
+const repository = require("./repository/todo");
+const todoService = require("./service/todo")(repository);
 
 const server = () => {
   const server = express();
   server.use(express.json());
   server.use(cors());
 
-  server.get('/api/todo', async (req, res) => {
+  server.get("/api/todo", async (req, res) => {
     res.json(await todoService.getTodos());
   });
 
-  /**
-  POST /api/todo
-  {
-   "task": "Some API"
-  }
-
-   {
-    "todos": [
-      {
-        "task": "Some API"
-      }
-    ]
-   }
-  **/
+  server.post("/api/todo", async (req, res) => {
+    const { task } = req.body;
+    if (!task) {
+      return res.status(400).json({ message: "Need to add a task" });
+    }
+    const postTodo = await todoService.addTodo(task);
+    res.status(201).json(postTodo);
+  });
 
   return server;
 };
