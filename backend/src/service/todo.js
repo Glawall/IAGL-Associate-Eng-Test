56b8faa9548b959620f290ea126b6bcba5050610
Glawall <glawall217@gmail.com>
2024-11-15 16:12:31 +0000

    Completes addTodo on api/todos endpoint

--- backend/src/service/todo.js
+++ backend/src/service/todo.js
@@ -1,9 +1,14 @@
 const todoService = (repository) => {
   return {
     getTodos: async () => {
-      return await repository.getTodos()
-    }
+      return await repository.getTodos();
+    },
+    addTodo: async (task) => {
+      if (!task) {
+        throw new Error("Task cannot be empty");
+      }
+      return await repository.addTodo(task);
+    },
   };
 };
-
 module.exports = todoService;
--- backend/test/repository/todo.test.js
+++ backend/test/repository/todo.test.js
@@ -1,14 +1,33 @@
+const repository = require("../../src/repository/todo");
 
-const repository = require('../../src/repository/todo');
-
-describe('TODO repository', () => {
-  it('should return the todo list', async () => {
+describe("getTodos", () => {
+  it("GET: 200 - getTodos should return the todo list", async () => {
     const expected = {
-      todos: [{
-        "task": "This is a todo example"
-      }]
+      todos: [
+        {
+          task: "This is a todo example",
+        },
+      ],
     };
     const actual = await repository.getTodos();
     expect(actual).toEqual(expected);
+  });
+});
+
+describe("addTodo", () => {
+  it("should add a new todo to the list and return the todo", async () => {
+    const newTodo = "Test the addTodo repository";
+    const expected = { task: newTodo };
+    const actual = await repository.addTodo(newTodo);
+    expect(actual).toEqual(expected);
+  });
+  it("if no task added, throw an error and reject invalid todo input", async () => {
+    const newTodo = "";
+    const expected = "Invalid todo input";
+    try {
+      await repository.addTodo(newTodo);
+    } catch (error) {
+      expect(error).toBe(expected);
+    }
   });
 });
